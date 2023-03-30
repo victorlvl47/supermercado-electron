319571caf0d6b4bc4c83cbb6e913b855d42bf3f5
victor <victorlvl47@gmail.com>
2023-03-30 16:50:33 -0600

    changed db name

--- supermercado-electron.sql
+++ supermercado-electron.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema supermercado-electron
+-- Schema supermercado_electron
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema supermercado-electron
+-- Schema supermercado_electron
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `supermercado-electron` DEFAULT CHARACTER SET utf8 ;
-USE `supermercado-electron` ;
+CREATE SCHEMA IF NOT EXISTS `supermercado_electron` DEFAULT CHARACTER SET utf8 ;
+USE `supermercado_electron` ;
 
 -- -----------------------------------------------------
--- Table `supermercado-electron`.`usuarios`
+-- Table `supermercado_electron`.`usuarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `supermercado-electron`.`usuarios` (
+CREATE TABLE IF NOT EXISTS `supermercado_electron`.`usuarios` (
   `id_usuario` INT NOT NULL AUTO_INCREMENT,
   `nombre_usuario` VARCHAR(45) NOT NULL,
   `numero_usuario` VARCHAR(15) NOT NULL,
@@ -29,6 +29,7 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 
 -- ------------------------------------------------------------------------
