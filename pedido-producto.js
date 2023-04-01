6778f7f3a35a76c31fd8cdbaf713c319e5b5b72e
victor <victorlvl47@gmail.com>
2023-03-31 18:45:20 -0600

    show list of providers to the user

--- pedido-producto.js
+++ pedido-producto.js
@@ -1,6 +1,27 @@
+
+function createSupplierSelect(suppliers) {
+    const select = document.createElement('select');
+    select.setAttribute('name', 'proveedores');
+    select.setAttribute('id', 'proveedores');
+  
+    suppliers.forEach((supplier) => {
+      const option = document.createElement('option');
+      option.setAttribute('value', supplier.nombre_proveedor);
+      option.textContent = supplier.nombre_proveedor;
+      select.appendChild(option);
+    });
+  
+    return select;
+}
+
+
 window.comunicacion.pedidoProducto(function(event, args) {
     
-    let proveedores_list = args;
+    // let proveedores_list = args;
+    var formLogin = document.getElementById('form-login');
+    const supplierNames = args;
+    const supplierSelect = createSupplierSelect(supplierNames);
+    formLogin.insertBefore(supplierSelect, formLogin.children[1]);
 });
 
 const cancelarBtn = document.getElementById('cancelar-btn');
